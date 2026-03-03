fccfd797de325c7f20972eddb1cc2eb80a003fc1
tirthbhalodia1305 <tirthbhalodia.1.3@gmail.com>
2026-03-03 11:39:05 +0530

    Update WordleSolver.cpp

--- WordleSolver.cpp
+++ WordleSolver.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <iterator>
+class WordleSolver {
 
+private:
+    std::vector<std::string> ignoreWords;
+    std::vector<std::string> board;
+public:
+    void display(const std::vector<std::string> currentBoard) {
+        std::cout << "[";
+        for (size_t i = 0; i < currentBoard.size(); ++i) {
+            std::cout << currentBoard[i];
+            if (i < currentBoard.size() - 1) {
+                std::cout << ", ";
+            }
+        }
+    std::cout << "]";
+    }
+        void resetBoard() {
+        board.clear();
+        for (int i = 0; i < 5; ++i) {
+            board.push_back("_");
+        }
+    }
+        void play() {
+        resetBoard();
+        display(board);
+    }
+};
+int main() {
+    WordleSolver solver;
+    solver.play();
+
+    return 0;
+}
