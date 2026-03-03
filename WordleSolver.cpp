8407b9ecdd6123c3217141a35f936327c76deddb
tirthbhalodia1305 <tirthbhalodia.1.3@gmail.com>
2026-03-03 11:45:39 +0530

    Update WordleSolver.cpp

--- WordleSolver.cpp
+++ WordleSolver.cpp
@@ -5,12 +5,50 @@
 #include <sstream>
 #include <algorithm>
 #include <iterator>
-class WordleSolver {
 
+class WordleSolver {
 private:
     std::vector<std::string> ignoreWords;
     std::vector<std::string> board;
+
 public:
+    void initializeBoard() {
+        int i = 0;
+        while (i < 5) {
+            i = userEntry(i);
+        }
+        display(board);
+        std::cout << std::endl;
+    }
+
+    int userEntry(int location) {
+        std::string initialState = board[location];
+        board[location] = "=";
+        display(board);
+        std::string userInput = getInput("");
+
+        if (userInput == " " || userInput.empty()) {
+            board[location] = initialState;
+            return location + 1;
+        } else if (userInput == "0") {
+            board[location] = initialState;
+            return 5;
+        } else if (userInput == "1") {
+            board[location] = "_";
+            return std::max(location - 1, 0);
+        } else {
+            board[location] = userInput;
+            return location + 1;
+        }
+    }
+
+    std::string getInput(const std::string prompt) {
+        std::cout << prompt << " ";
+        std::string input;
+        std::getline(std::cin, input);
+        return input;
+    }
+
     void display(const std::vector<std::string> currentBoard) {
         std::cout << "[";
         for (size_t i = 0; i < currentBoard.size(); ++i) {
@@ -19,22 +57,27 @@ public:
                 std::cout << ", ";
             }
         }
-    std::cout << "]";
+        std::cout << "]";
     }
-        void resetBoard() {
+
+    void resetBoard() {
         board.clear();
-        for (int i = 0; i < 5; ++i) {
+        for (int i = 0; i < 5; i++) {
             board.push_back("_");
         }
     }
-        void play() {
+
+    void play() {
+        std::string prompt = "Your current cell is marked with a '='.\n * Enter a letter if you know it belongs in that cell.\n * Enter a space if you want to skip to the next cell.\n * Enter '1' to move one cell back and make changes.\n * Enter '0' to exit the board editing process.";
+        std::cout << prompt << std::endl;
+        
         resetBoard();
-        display(board);
+        initializeBoard();
     }
 };
+
 int main() {
     WordleSolver solver;
     solver.play();
-
     return 0;
 }
